76f15b763b366643b67a0e422ac96c03e022645a
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2026-03-04 08:41:47 +0600

    Project Initialized

---
+++ src/main/java/com/example/github_repo_reader/GithubRepoReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_repo_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoReaderApplication.class, args);
+	}
+
+}
